ba215fef7a5ec474c78c7cd5ecf8f5044833f1f8
Isabella Flauzino <isabella.oliveira2009@gmail.com>
2023-10-02 13:58:49 -0300

    update

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,3 +1,33 @@
-const formInputs = document.querySelectorAll("form input");
+const formInputs = document.querySelectorAll(".c-modal input");
+const formInputs = document.getElementById("user_input_login_modal")
+const formInputs = document.getElementById("password_input_login_modal");
+const formInputs = document.getElementById("email_input_recover_password_modal");
+const formInputs = document.getElementById("tax_indentifier_input_establishment_modal");
 
-console.log(formInputs);
+formInputs.forEach((element) => {
+  console.log(element);
+  element.classList.add("u-input-correct");
+  element.classList.add("u-input-error");
+});
+
+Function checkInputs() {
+  const user_input_login_modal = username.value;
+  const password_input_login_modal = passaword.value;
+  const email_input_recover_password_modal = email.value
+  const tax_indentifier_input_establishment_modal = tax.value
+
+  if (user_input_login_modal == '') {
+    setErrorFor(user_input_login_modal, "Username is required."); 
+  } else {
+    SetSucessFor(user_input_login_modal);
+  }
+} 
+
+Function setErrorFor (user_input_login_modal, message)[
+  const formInputs = user_input_login_modal.parenteElement;
+  const small = formInputs.querySelector("small");
+
+  small.innerText = message;
+
+  formInputs.className = "formInputs.error";
+  
